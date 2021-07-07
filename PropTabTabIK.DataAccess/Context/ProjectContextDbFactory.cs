aa1d1155d70c404b301e1f1f0c53ca838c179c69
Fatma Gurel <fatma.gurel.0@gmail.com>
2021-07-07 15:26:11 +0300

    Initial commit

---
+++ PropTabTabIK.DataAccess/Context/ProjectContextDbFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PropTabTabIK.DataAccess.Context
+{
+    public class ProjectContextDbFactory : IDesignTimeDbContextFactory<ProjectContext>
+    {
+        public ProjectContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ProjectContext>();
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-19NARKN;Database=ScrumDB;Trusted_Connection=true");
+
+            return new ProjectContext(optionsBuilder.Options);
+        }
+    }
+}
